28d76720f58f9bda8e289aa25f33fb1e68883c42
Sajjad Ahmad <jogimar14@gmail.com>
2023-01-10 20:50:32 +0500

    Add routes, controller actions, and views for users controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,15 @@
+class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def index
+    @users = User.all
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :photo, :bio, :posts_counter)
+  end
+end
